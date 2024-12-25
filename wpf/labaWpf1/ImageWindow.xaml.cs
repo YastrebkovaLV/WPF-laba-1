1ac24e3c3ee426b6de9aeb716c30fcae2ceb233d
Lilechka_ <kekovichl436@gmail.com>
2024-12-25 12:37:49 +0300

    Add files via upload

---
+++ wpf/labaWpf1/ImageWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace imageWindow
+{
+    public partial class ImageWindow : Window
+    {
+        public ImageWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
+
---
+++ wpf/labaWpf1/knopki.xaml.cs
@@ -0,0 +1,19 @@
+﻿using imageWindow;
+using System.Windows;
+
+namespace wpf
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            ImageWindow imageWindow = new ImageWindow();
+            imageWindow.Show();
+        }
+    }
+}
